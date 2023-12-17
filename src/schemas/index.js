27646512fbf4c620fa4469f03b94c0f12357025e
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2023-12-17 11:47:19 +0500

    finish basic form

---
+++ src/schemas/index.js
@@ -0,0 +1,14 @@
+import * as yup from 'yup';
+
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
+
+export const basicSchema = yup.object().shape({
+  email: yup.string().email("Pleas enter a valid email").required("Required"),
+  age: yup.number().positive().integer().required("Required"),
+  password: yup
+    .string()
+    .min(5)
+    .matches(passwordRules, { message: "Please create strong password" })
+    .required("Required"),
+  confirmPassword: yup.string().oneOf([yup.ref("password"), null]),
+});
